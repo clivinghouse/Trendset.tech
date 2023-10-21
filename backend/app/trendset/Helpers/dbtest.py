5ffef1167daa0b264938c1bb28426b3a78e54d4e
clivinghouse <79345446+clivinghouse@users.noreply.github.com>
2023-10-21 18:20:05 -0400

    updated and moved stuff

--- backend/app/trendset/Helpers/dbtest.py
+++ backend/app/trendset/Helpers/dbtest.py
@@ -1,4 +1,4 @@
-from dbHelperclass import dbHelper as dbh
+from backend.app.trendset.Helpers.dbHelperclass import dbHelper as dbh
 
 conn = dbh("mongodb+srv://clivinghouse:Mason121302!!@trendset.plb5zxd.mongodb.net/","trendset")
 out = conn.getUser(1234)

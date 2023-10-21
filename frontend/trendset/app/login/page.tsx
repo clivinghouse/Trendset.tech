ac3df07c273fc2c7231d0d2b2fc08decb82fdafa
Dradeon <69202556+Dradeon@users.noreply.github.com>
2023-10-21 19:29:31 -0400

    Added more fields to Sign up and provided stylings

--- frontend/trendset/app/login/page.tsx
+++ frontend/trendset/app/login/page.tsx
@@ -35,18 +35,26 @@ const login = () => {
 
   return (
     <div>
-        <a href="/">Home</a>
-        <h1>Login</h1>
-        <form onSubmit={login}>
-        <label htmlFor='email'>
-                <input required type='email' name='email' id='email' onChange={(e)=>setEmail(e.target.value)}></input>
-            </label>
-            <label htmlFor='password'>
-                <input required type='password' name='email' id='password' onChange={(e) => setPassword(e.target.value)}></input>
-            </label>
-            <button type="submit">Login</button>
-        </form>
-        <span><p>Don't have an account?</p><a href="sign_up">Create an account.</a></span>
+        <nav className="flex justify-between">
+          <a href='/'><h1 className='text-4xl font-bold'>TrendSet.tech</h1></a>
+        </nav>
+        <div className='mt-4 mb-4 flex flex-col'>
+          <h1 className='text-4xl font-bold text-center mt-8 mb-8'>Login</h1>
+          <form onSubmit={login} className='m-auto mb-6'>
+              <label htmlFor='email'>
+                  <p>Email:</p>
+                  <input className='border mb-4' required type='email' name='email' id='email' onChange={(e)=>setEmail(e.target.value)}></input>
+              </label>
+              <label htmlFor='password'>
+                  <p>Password:</p>
+                  <input className='border mb-4' required type='password' name='email' id='password' onChange={(e) => setPassword(e.target.value)}></input>
+              </label>
+              <div className='flex justify-center'>
+                <button type="submit" className='pt-2 pb-2 pl-4 pr-4 self-center bg-green-500 font-bold rounded-md'>Sign up</button>
+              </div>
+          </form>
+          <span className='m-auto text-center'><p>Already have an account?</p> <a href = "/login" className='underline'>Login into your account.</a></span>
+        </div>
     </div>
   )
 }
--- frontend/trendset/app/page.tsx
+++ frontend/trendset/app/page.tsx
@@ -1,10 +1,23 @@
+'use client'
+import { useAuthContext } from "@/app/_components/AuthProvider"
+import { User } from "firebase/auth"
 
 export default function Home() {
+  const user : User | null = useAuthContext()
+
   return (
-    <main className="">
-      <h1>TrendSet.tech</h1>
-      <a href="/sign_up">Sign Up</a>
-      <a href="/login">Login</a>
+    <main>
+      <nav className="flex justify-between">
+        <h1>TrendSet.tech</h1>
+        <div className="flex justify-end gap-4">
+          {user && <a href="/dashboard">Dashboard</a>}
+          <a href="/sign_up">Sign Up</a>
+          <a href="/login">Login</a>
+        </div>
+      </nav>
+      
+      
+
     </main>
   )
 }

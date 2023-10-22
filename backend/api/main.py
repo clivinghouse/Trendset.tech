1d7d72c24fa99411f128613d228edc098bae382e
bmattblake <bmattblake@gmail.com>
2023-10-21 23:51:26 -0400

    rename getUser

--- backend/api/main.py
+++ backend/api/main.py
@@ -7,12 +7,14 @@ app = Flask(__name__)
 conn = dbh("mongodb+srv://trendset:FIfRuK42erNOir02@trendset.plb5zxd.mongodb.net","trendset")
 
 @app.route("/api/getUser/<email>")
-def hello(email):
+def getUser(email):
     connResp = conn.getUser(email)
     if connResp is None:
         return("null")
     else:
         return json_util.dumps(connResp)
+    
+
 
 
 @app.route("/")

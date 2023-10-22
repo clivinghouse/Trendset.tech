e5b8598361a60d453339439db0b2d81701381f55
clivinghouse <79345446+clivinghouse@users.noreply.github.com>
2023-10-21 22:28:58 -0400

    db connector complete

--- backend/app/trendset/Helpers/dbHelperclass.py
+++ backend/app/trendset/Helpers/dbHelperclass.py
@@ -5,31 +5,67 @@ class dbHelper:
     def __init__(self,dbConnectionString, database):
         self.client = pymongo.MongoClient(dbConnectionString)[database]
 
-    def getUser(self, id):
+    def getUser(self, email):
         
         col = self.client["users"]
         ret = col.find_one({
-            "firebase_id":id
+            "email":email
         })
         return ret
     
-    def addUser(self,firebase_id,f_name,l_name,email):
-        results = self.getUser(firebase_id)
+    def addUser(self,user):
+        results = self.getUser(user['email'])
 
         if results is None:
             col = self.client["users"]
-            newUser = {
-                "firebase_id":firebase_id,
-                "f_name":f_name,
-                "l_name":l_name
-            }
-            col.insert_one(newUser)
-            return newUser
+            
+            col.insert_one(user)
+            return user
                  
         else:
             
             return None
-    
+    def getUniqueProduct(self,id):
+        col = self.client['products']
+        prods = col.find_one({
+            "product.id":id
+        })
+        return prods
+    def biggestVal(self):
+        col = self.client['products']
+        prods = col.find({},{'product.id':1})
+        largest = 0
+        for num in prods:
+            if num['product']['id'] > largest:
+                largest = num['product']['id']
+        return largest
+    def addProduct(self,email,product):
+        biggestVal = self.biggestVal()
+        product.update({"id":biggestVal+1})
+        col = self.client["products"]
+        fCol = col.find_one({"email":email})
+        ret = col.insert_one({
+                "email":email,
+                "product":product
+            })
+        return ret
+    def removeProduct(self,id):
+        object = self.getUniqueProduct(id)
+        col = self.client['products']
+        rem = col.delete_many({'product.id':id})
+        print(rem)
+        return object
+    def updateProduct(self,id,product):
+        col = self.client['products']
+        myquery = { "product.id": id }
+        newvalues = { "$set": product }
+        col.update_many(myquery, newvalues)
+        return self.getUniqueProduct(id)
         
+
+            
+               
+        
+
     
     
--- backend/app/trendset/Helpers/dbtest.py
+++ backend/app/trendset/Helpers/dbtest.py
@@ -1,6 +1,29 @@
 from  dbHelperclass import dbHelper as dbh
 
 conn = dbh("mongodb+srv://trendset:FIfRuK42erNOir02@trendset.plb5zxd.mongodb.net/","trendset")
-out = conn.getUser(1234)
-print(out)
-print(conn.addUser(111,"Corey","liv","co@c.com"))
+# conn.getUniqueProduct()
+print(conn.biggestVal())
+# out = conn.getUser(1234)
+# print(out)
+# print(conn.addUser({"f_name":"Corey","l_name":"liv","email":"co@c.com"}))
+
+# removed = conn.removeProduct(1)
+upDateProduct = {
+    "logo":"https://google.com",
+    "bio":"this is not a test",
+    "links":[{
+        "text":"linkdin",
+        "url":"https://google.com",
+        
+}],
+"design":{
+            "card_color":"blue",
+            "window_color":"green",
+            "text-font":"sans",
+            "text_color":"red"
+        }
+    } 
+added = conn.addProduct("co@@c.com",upDateProduct)
+
+# print(conn.updateProduct(3,upDateProduct))
+

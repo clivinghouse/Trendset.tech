6861f724fdff5026982e1ab6a7d3dbf3ad28d43d
clivinghouse <79345446+clivinghouse@users.noreply.github.com>
2023-10-22 02:34:30 -0400

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -56,7 +56,10 @@ def getAllForUser(email):
     return result
     
 
-
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)

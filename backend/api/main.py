1093b052442d3023f7518abd3ce112f55024e4f7
bmattblake <bmattblake@gmail.com>
2023-10-22 00:29:42 -0400

    added flask

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from markupsafe import escape
 from Helpers.dbHelperclass import dbHelper as dbh
 from bson import json_util
@@ -14,8 +14,28 @@ def getUser(email):
     else:
         return json_util.dumps(connResp)
     
+@app.route("/api/addUser/<user>")
+def addUser(user):
+    response = request.data
+    new_user = conn.addUser(response)
+    
+        
+    return json_util.dumps(new_user)
 
-
+@app.route("/api/getUniqueProd/<id>")
+def getUniqueProd(id):
+    connResp = conn.getUser(id)
+    if connResp is None:
+        return("null")
+    else:
+        return json_util.dumps(connResp)
+    
+@app.route("/api/addProduct/<email><product>")
+def addProduct(email, product):
+    response = request.data
+    email = response
+    new_prod = conn.addProduct(email, product)
+    return new_prod
 
 @app.route("/")
 def hello_world():
